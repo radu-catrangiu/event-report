20212448a3cdd7c730b10071a70970563b31dec2
Radu Ovidiu CATRANGIU <rcatrangiu@bitdefender.com>
2019-12-23 15:18:28 +0200

    Update NewEventCard

--- frontend/src/components/NewEventCard.vue
+++ frontend/src/components/NewEventCard.vue
@@ -12,6 +12,39 @@
             v-model="event.title"
           />
         </div>
+        <div class="form-group mb-3">
+          <div>
+            <label>Event Type</label>
+          </div>
+          <select class="custom-select" v-model="event.tag">
+            <option selected value>Choose...</option>
+            <option
+              v-for="eventType in eventTypes"
+              :key="eventType"
+              :value="eventType"
+            >{{eventType | capitalize}}</option>
+          </select>
+        </div>
+
+        <div class="form-group">
+          <label for="eventDescriptionTextarea">Event Description</label>
+          <textarea
+            class="form-control"
+            id="eventDescriptionTextarea"
+            rows="3"
+            v-model="event.description"
+          ></textarea>
+        </div>
+
+        <div class="form-group mb-3">
+          <div>
+            <label>Event Picture</label>
+          </div>
+          <div class="custom-file">
+            <input type="file" class="custom-file-input" id="photoInput" @change="onFileChange" />
+            <label class="custom-file-label" for="photoInput">{{filename || "Choose file"}}</label>
+          </div>
+        </div>        
         <div class="form-group">
           <div>
             <label for="bla">Pick Location of Event</label>
@@ -31,15 +64,6 @@
             v-model="location"
             disabled
           />
-        </div>
-        <div class="form-group">
-          <label for="eventDescriptionTextarea">Event Description</label>
-          <textarea
-            class="form-control"
-            id="eventDescriptionTextarea"
-            rows="3"
-            v-model="event.description"
-          ></textarea>
         </div>
 
         <button type="button" class="btn btn-danger btn-lg" @click="reportEvent">REPORT EVENT</button>
@@ -67,14 +91,38 @@ export default {
       event: {
         title: "",
         description: "",
+        tag: "",
         location: {
           lat: null,
           lng: null
-        }
-      }
+        },
+        image: ""
+      },
+      filename: undefined,
+      eventTypes: ["bla", "one", "two"]
     };
   },
+  filters: {
+    capitalize(str) {
+      let x = str;
+
+      return x.charAt(0).toUpperCase() + x.substring(1);
+    }
+  },
   methods: {
+    onFileChange(e) {
+      var file = event.target.files[0];
+      if (!file) return;
+      var reader = new FileReader();
+      reader.onload = e => {
+        // console.log(e.target.result.split(','));
+        const imageData = e.target.result.split(",");
+        this.event.image = imageData[1];
+      };
+
+      reader.readAsDataURL(file);
+      console.log(file);
+    },
     getBrowserLocation() {
       if (!navigator.geolocation) {
         return;
@@ -94,9 +142,9 @@ export default {
       if (title.length < 3 || description.length < 10 || !lat || !lng) {
         return;
       }
-      const result = await this.$api.post('/events', this.event);
+      const result = await this.$api.post("/events", this.event);
       const storedEvent = result.data;
-      this.$parent.eventsList.unshift(storedEvent)
+      this.$parent.eventsList.unshift(storedEvent);
     }
   }
 };
