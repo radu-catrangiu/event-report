9377eca5098dbfa2da34c0821fb7de2b26392320
radu-catrangiu <radu.catrangiu@gmail.com>
2019-12-26 21:31:24 +0200

    Add error message to login screen

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,62 +1,56 @@
 <template>
   <div>
-      <div v-if="user">
-          <!-- <span class="mx-2">User Name</span> -->
-          <button class="btn btn-secondary" @click="logOut">Log Out</button>
-      </div>
-      <div v-else>
-          <button class="btn btn-secondary" @click="openLoginModal">Log In</button>
-      </div>
+    <div v-if="user">
+      <button class="btn btn-secondary" @click="logOut">Log Out Admin</button>
+    </div>
+    <div v-else>
+      <button class="btn btn-secondary" @click="openLoginModal">Log In Admin</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    name: "Login",
-    data() {
-        return {
-            user: null
-        }
-    },
-    async mounted() {
-        this.$store.subscribe(mutation => {
-            if (mutation.type !== "user") return;
-            this.user = this.$store.getters.user;
+  name: "Login",
+  data() {
+    return {
+      user: null
+    };
+  },
+  async mounted() {
+    this.$store.subscribe(mutation => {
+      if (mutation.type !== "user") return;
+      this.user = this.$store.getters.user;
     });
-        const loginToken = this.$cookies.get('login_token');
-        if(loginToken) {
-            try {
-                const result = await this.$api.get('/auth/token', { 
-                    params: {
-                        login_token: loginToken
-                    }
-                });
-                if (result.status === 200, result.data) {
-                    this.$store.commit('user', result.data);
-                    this.user = result.data;
-                }
-
-            } catch (error) {
-                // eslint-disable-next-line
-                console.error(error);
-                this.$store.commit("user", null);
-                this.$cookies.remove("login_token");
-            }
-            
+    const loginToken = this.$cookies.get("login_token");
+    if (loginToken) {
+      try {
+        const result = await this.$api.get("/auth/token", {
+          params: {
+            login_token: loginToken
+          }
+        });
+        if ((result.status === 200, result.data)) {
+          this.$store.commit("user", result.data);
+          this.user = result.data;
         }
-    },
-    methods: {
-        logOut() {
-            this.$store.commit("user", null);
-            this.$cookies.remove("login_token");
-        },
-        openLoginModal() {
-            this.$EventBus.$emit('open-login-modal');
-        }
+      } catch (error) {
+        this.$store.commit("user", null);
+        this.$cookies.remove("login_token");
+      }
     }
-}
+  },
+  methods: {
+    logOut() {
+      this.$store.commit("user", null);
+      this.$cookies.remove("login_token");
+    },
+    openLoginModal() {
+      this.$EventBus.$emit("open-login-modal");
+    }
+  }
+};
 </script>
 
 <style>
-
 </style>
