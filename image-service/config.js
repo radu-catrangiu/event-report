78061069f171a417795deae63a475cb6fe806f55
radu-catrangiu <radu.catrangiu@gmail.com>
2019-12-28 21:54:29 +0200

    Update service configs to use env vars

--- image-service/config.js
+++ image-service/config.js
@@ -1,11 +1,11 @@
 module.exports = {
     port: process.env.PORT || 8082,
     mongo: {
-        host: "ds125693.mlab.com",
-        port: "25693",
-        db: "event_report",
-        user: "padawan",
-        password: "123Parola",
+        host: process.env.MONGO_HOST || "ds125693.mlab.com",
+        port: process.env.MONGO_PORT || "25693",
+        db: process.env.MONGO_DB || "event_report",
+        user: process.env.MONGO_USER || "padawan",
+        password: process.env.MONGO_PWD || "123Parola",
         collections: ['images']
     }
 };
