3b66465f007399d90efd3bfe1d633b0f9c4d24e0
radu-catrangiu <radu.catrangiu@gmail.com>
2020-01-05 22:14:29 +0200

    Add Swarm support

---
+++ run-stack.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker build -t cc-frontend-rcatrangiu ./frontend
+docker build -t cc-backend-rcatrangiu ./backend
+docker build -t cc-image-service-rcatrangiu ./image-service
+
+docker stack deploy -c docker-stack.yaml cc
