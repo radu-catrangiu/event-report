75515625fc7d18b9bd31da33b44cd7d6afd79902
radu-catrangiu <radu.catrangiu@gmail.com>
2020-01-05 22:59:07 +0200

    Remove the need for two yaml files

--- run-stack.sh
+++ run-stack.sh
@@ -4,4 +4,4 @@ docker build -t cc-frontend-rcatrangiu ./frontend
 docker build -t cc-backend-rcatrangiu ./backend
 docker build -t cc-image-service-rcatrangiu ./image-service
 
-docker stack deploy -c docker-stack.yaml cc
+docker stack deploy -c docker-compose.yaml cc
